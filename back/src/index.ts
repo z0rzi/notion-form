dbc18303ffa2dc271ec942202cb85742b8f77332
Brian Joseph <brianjosephstudio@gmail.com>
2024-04-13 23:46:26 +0200

    Integrated Auth0

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,11 +3,23 @@ import express, { Request, Response } from 'express';
 import routes from './routes';
 import PromptDeamon from './PromptDeamon';
 import { resetDb } from './Db';
+import { auth } from 'express-openid-connect';
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:8080',
+  clientID: 'N7MMCbK3Ir1uuSeGvRwu3W1xSyT5PNeS',
+  issuerBaseURL: 'https://dev-wpunndg44r5bjef6.us.auth0.com'
+};
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(auth(config));
 
 // To get IP address of the client
 app.set('trust proxy', true);
