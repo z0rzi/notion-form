12357e12c6c620a3b91d22fffcfa801a4b5966c9
Brian Joseph <brianjosephstudio@gmail.com>
2024-04-21 17:53:16 +0200

    cleaned up proxyMiddleware

--- back/src/devProxy.ts
+++ back/src/devProxy.ts
@@ -2,14 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 import { Request, Response, NextFunction } from 'express';
 
 export const useProxyIfDev = () => {
-    console.log('oyi');
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.USE_VITE_SERVER);
     if (
         process.env.NODE_ENV === 'development' &&
         process.env.USE_VITE_SERVER === 'true'
     ) {
-        console.log('redirecting to proxy');
         const proxy = createProxyMiddleware({
             target: process.env.VITE_DEV_SERVER,
             changeOrigin: true,
@@ -18,7 +14,6 @@ export const useProxyIfDev = () => {
         return proxy;
     }
     return (_req: Request, _res: Response, next: NextFunction) => {
-        console.log('production build');
         next();
     };
 };
